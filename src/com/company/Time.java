c71bbc07c20146c046e6ce0851cc52e71566bba0
AndraM2020 <andramozga@gmail.com>
2021-09-19 18:54:02 +0300

    Edited

--- src/com/company/Time.java
+++ src/com/company/Time.java
@@ -4,19 +4,15 @@ import java.util.Scanner;
 
 public class Time {
     public static void main(String[] args) {
-        short time = -2;
-        if (time < 12 && time > 0) {
+        short time = 4;
+        if (time < 12 && time > 0)
             System.out.println("Good Morning Sunshine!");
-        }
-        if (time >= 13 && time <= 19) {
+        if (time >= 13 && time <= 19)
             System.out.println("Good Afternoon! Work Hard!");
-        }
-        if (time >= 20 && time <= 24) {
+        if (time >= 20 && time <= 24)
             System.out.println("Good Evening! Get some rest!");
-        }
-        if (time < 0 || time > 24) {
+        if (time < 0 || time > 24)
             System.out.println("Please choose value in 24 hour time format");
-        }
 
     }
 }
